06cd62c44f76a4c24896f89cf2065337f68568cf
Lolomyn <61377364+Lolomyn@users.noreply.github.com>
2026-09-17 15:50:39 +0300

    v51.0.1

--- laserhall-axiom.user.js
+++ laserhall-axiom.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Laserhall Axiom v48
+// @name         Laserhall Axiom
 // @namespace    https://laserhall.simprint.pro/
-// @version      50.0.0
+// @version      51.0.1
 // @description  
 // @match        https://laserhall.simprint.pro/axiom/index_postpress.php
 // @grant        none
@@ -731,7 +731,7 @@
             const h = document.createElement('div');
             h.style.cssText = 'padding:10px 14px;background:#f5f5f5;border-bottom:1px solid #ddd;display:flex;align-items:center;justify-content:space-between;font:600 15px Arial;flex-shrink:0;';
             const ti = document.createElement('span');
-            ti.textContent = 'Работа участка ШФ [Stable Version]';
+            ti.textContent = 'Работа участка ШФ';
 
             const r = document.createElement('div');
             r.style.cssText = 'display:flex;gap:8px;align-items:center;';
